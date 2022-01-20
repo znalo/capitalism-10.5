11c6937e6558e8b67088a343cafd5d6f01a29760
Alan Freeman <afreeman@iwgvt.org>
2022-01-20 14:54:24 -0600

    removed unused installs

--- capitalism/capitalism/settings.py
+++ capitalism/capitalism/settings.py
@@ -38,10 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'rest_framework_datatables',
     'economy',
-    'import_export',
 ]
 
 MIDDLEWARE = [
--- capitalism/economy/admin.py
+++ capitalism/economy/admin.py
@@ -1,81 +1,15 @@
 from django.contrib import admin
 from .models.states import Log, Project, TimeStamp
 from .models.commodity import Commodity
-from .models.owners import StockOwner, Industry, SocialClass
+from .models.owners import Industry, SocialClass
 from .models.stocks import SocialStock, State, IndustryStock
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
 
-class CommodityResource(resources.ModelResource):
-   class Meta:
-      model = Commodity
- 
-class CommodityAdmin(ImportExportModelAdmin):
-   resource_class = CommodityResource
-
-class ProjectResource(resources.ModelResource):
-   class Meta:
-      model = Project
- 
-class ProjectAdmin(ImportExportModelAdmin):
-   resource_class = ProjectResource
-
-class TimeStampResource(resources.ModelResource):
-   class Meta:
-      model = TimeStamp
- 
-class TimeStampAdmin(ImportExportModelAdmin):
-   resource_class = TimeStampResource   
-
-class IndustryResource(resources.ModelResource):
-   class Meta:
-      model = Industry
- 
-class IndustryAdmin(ImportExportModelAdmin):
-   resource_class = IndustryResource   
-   
-class SocialClassResource(resources.ModelResource):
-   class Meta:
-      model = SocialClass
- 
-class SocialClassAdmin(ImportExportModelAdmin):
-   resource_class = SocialClassResource   
-
-class SocialStockResource(resources.ModelResource):
-   class Meta:
-      model = SocialStock
- 
-class SocialStockAdmin(ImportExportModelAdmin):
-   resource_class = SocialStockResource   
-      
-class IndustryStockResource(resources.ModelResource):
-   class Meta:
-      model = IndustryStock
- 
-class IndustryStockAdmin(ImportExportModelAdmin):
-   resource_class = IndustryStockResource   
-
-class StateResource(resources.ModelResource):
-   class Meta:
-      model = State
- 
-class StateAdmin(ImportExportModelAdmin):
-   resource_class = StateResource   
-
-class LogResource(resources.ModelResource):
-   class Meta:
-      model=Log
-
-class LogAdmin(ImportExportModelAdmin):
-   resource_class=LogResource
-
-
-admin.site.register(Commodity,CommodityAdmin)
-admin.site.register(Project,ProjectAdmin)
-admin.site.register(TimeStamp,TimeStampAdmin)
-admin.site.register(Industry,IndustryAdmin)
-admin.site.register(SocialClass,SocialClassAdmin)
-admin.site.register(SocialStock,SocialStockAdmin)
-admin.site.register(IndustryStock,IndustryStockAdmin)
-admin.site.register(State,StateAdmin)
-admin.site.register(Log,LogAdmin)
+admin.site.register(Commodity)
+admin.site.register(Project)
+admin.site.register(TimeStamp)
+admin.site.register(Industry)
+admin.site.register(SocialClass)
+admin.site.register(SocialStock)
+admin.site.register(IndustryStock)
+admin.site.register(State)
+admin.site.register(Log)
